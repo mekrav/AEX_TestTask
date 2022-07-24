716bf1a25bc1bb8034363bfc8828063ce698130b
mekrav <mekrav@gmail.com>
2022-07-24 22:02:13 +0700

    Day 4 progress

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,26 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
+    constructor(props) {
+        super(props);
+        this.state = { movies: [], loading: true, value: '' };
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        this.moviesData();
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        this.moviesData();
+    }
+
+    renderMoviesTable(movies) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Title</th>
+                        <th>Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map(movie =>
+                        <tr key={movie.id}>
+                            <td>{movie.id}</td>
+                            <td>{movie.title}</td>
+                            <td>{movie.year}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : this.renderMoviesTable(this.state.movies);
+
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input name ="searchRequest" type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                {contents}
+            </div>
+        );
+    }
+    
+    async moviesData() {
+        const response = await fetch('movielist/' + this.state.value);
+        const data = await response.json();
+        this.setState({ movies: data, loading: false});
+    }
   }
-}
---
+++ Controllers/MovieListController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieSearcher.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieSearcher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieListController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<MovieListController> _logger;
+
+        public MovieListController(ILogger<MovieListController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            Movie[] movieList;
+            using (var context = new ApplicationDbContext())
+            {
+                context.Database.EnsureCreated();
+                movieList = context.Movies.ToArray();
+            }
+            return movieList;
+        }
+
+        [HttpGet("{searchRequest}")]
+        public IEnumerable<Movie> Get(string searchRequest)
+        {
+            Movie[] movieList;
+            using (var context = new ApplicationDbContext())
+            {
+                movieList = context.Movies.ToArray();
+            }
+            return movieList;
+        }
+    }
+}
